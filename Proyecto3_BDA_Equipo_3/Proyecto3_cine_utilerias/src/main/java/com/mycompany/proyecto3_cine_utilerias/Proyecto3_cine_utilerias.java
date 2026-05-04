a48d308124d9a6ace19f633cbd027c939ba8114c
adrian252699 <jesus.luzanilla252699@potros.itson.edu.mx>
2026-05-04 12:31:49 -0700

    Proyecto y dependencias

---
+++ Proyecto3_BDA_Equipo_3/Proyecto3_cine_utilerias/src/main/java/com/mycompany/proyecto3_cine_utilerias/Proyecto3_cine_utilerias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto3_cine_utilerias;
+
+/**
+ *
+ * @author jalt2
+ */
+public class Proyecto3_cine_utilerias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
